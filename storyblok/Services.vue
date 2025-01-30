97f794c926077aa7b82f3168a812e97c9131ef8e
nico-sup <jeanne-nicolas@laposte.net>
2025-01-30 09:02:39 +0100

    update: services blok

--- storyblok/Services.vue
+++ storyblok/Services.vue
@@ -1,14 +1,16 @@
 <template>
-  <section v-editable="blok" class="container border-y border-y-green-ascent py-5 layout-grid">
-    <div class="col-span-3 grid grid-cols-3 items-start gap-3">
-        <img :src="blok.img.filename" alt="" class="w-full h-[15rem] object-cover col-span-2">
-        <p class="mt-0 text-green-ascent font-bold">{{ blok.number }}</p>
-    </div>
-
-    <div class="col-span-6 flex flex-col">
-        <h2 class="font-chewy flex-1">{{ blok.title }}</h2>
-        <h3 class="text-[1rem] bg-green-ascent w-fit text-white px-2">{{ blok.tag }}</h3>
-
+  <section v-editable="blok" class="border-t border-t-green-ascent services my-0">
+    <div class="container layout-grid py-5 my-0">
+        <div class="col-span-3 hidden md:grid grid-cols-3 items-start gap-3">
+            <img :src="blok.img.filename" alt="" class="w-full h-[15rem] object-cover col-span-2 rounded-3xl">
+            <p class="mt-0 text-green-ascent font-bold">{{ blok.number }}</p>
+        </div>
+    
+        <div class="col-span-6 flex flex-col">
+            <h2 class="font-chewy flex-1">{{ blok.title }}</h2>
+            <h3 class="text-[1rem] bg-green-ascent w-fit text-white px-2">{{ blok.tag }}</h3>
+    
+        </div>
     </div>
 
   </section>
--- storyblok/Split.vue
+++ storyblok/Split.vue
@@ -1,12 +1,12 @@
 <template>
     <section class="layout-grid split">
 
-        <div class="col-span-6 max-h-[30vh] md:max-h-none md:col-span-7 bg-red-400 col-start-1 h-screen">
-            <img :src="blok.img_large.filename" alt="" class="h-full w-full object-cover">
+        <div class="col-span-6 max-h-[30vh] md:max-h-none md:col-span-7 col-start-1 h-screen">
+            <img :src="blok.img_large.filename" alt="" class="h-full w-full object-cover md:rounded-tr-3xl md:rounded-br-3xl">
         </div>
 
         <div class="col-span-6 md:col-span-4 grid">
-            <img :src="blok.img_small.filename" alt="" class="h-40 w-40 md:w-20 mb-5 md:mb-0 object-cover">
+            <img :src="blok.img_small.filename" alt="" class="h-40 w-40 md:w-20 mb-5 md:mb-0 object-cover md:rounded-3xl">
             <div class="flex flex-col items-center max-w-[80%] md:max-w-[60%] mx-auto">
                 <h2> {{ blok.title }}</h2>
                 <p>{{ blok.description }}</p>
