f1e4b51d11e20aab7b73b6590306e0535949fd44
nico-sup <jeanne-nicolas@laposte.net>
2025-02-22 09:40:04 +0100

    feat: bloc video section

---
+++ storyblok/VideoBloc.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-editable="blok" class="flex flex-col items-center border-t-2 pt-4 border-t-black video-bloc p-4">
+        <span class="text-right w-full">({{ blok.number }})</span>
+        <h3 class="text-center">{{ blok.title }}</h3>
+        <div class="w-[60vw] h-[30vw] md:w-[32vw] md:h-[20vw] overflow-hidden">
+            <video autoplay muted loop playsinline class="h-full w-full object-cover scale-125" >
+                <source :src="blok.asset.filename" />
+          </video>
+        </div>
+      <div class="flex w-full justify-between mt-6 px-6">
+        <h4 class="text-xs">{{ blok.tag_one }}</h4>
+        <h4 class="text-xs">{{ blok.tag_two }}</h4>
+        <h4 class="text-xs">{{ blok.tag_three }}</h4>
+      </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
---
+++ storyblok/VideoSection.vue
@@ -0,0 +1,16 @@
+<template>
+    <section v-editable="blok" class="container-full layout-grid video-section">
+        <div class="col-span-full">
+            <ul class="grid md:grid-cols-2">
+                <li v-for="(item, index) in blok.items" :key="index">
+                    <VideoBloc :blok="item" :index="index" />
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import VideoBloc from '~/storyblok/VideoBloc.vue';
+defineProps({ blok: Object });
+</script>
