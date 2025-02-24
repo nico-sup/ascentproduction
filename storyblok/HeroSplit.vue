cdbf80ad8ccf12af70e5c1c2acc4660817708f97
nico-sup <jeanne-nicolas@laposte.net>
2025-02-24 12:30:36 +0100

    fix: size text list

--- storyblok/HeroSplit.vue
+++ storyblok/HeroSplit.vue
@@ -3,13 +3,13 @@
         <div v-editable="blok" class="layout-grid hero-split !gap-0">
       
           <div class="col-span-full md:col-span-8 hero-split__left">
-              <img class="bg-img" v-if="blok.bg_left" :src="blok.bg_left.filename" :alt="blok.bg_left.alt" alt="">
+              <img class="bg-img rounded-none" v-if="blok.bg_left" :src="blok.bg_left.filename" :alt="blok.bg_left.alt" alt="">
               <img class="z-[1] img-left" v-if="blok.img_left" :src="blok.img_left.filename" :alt="blok.img_left.alt" alt="">
               <span>{{ blok.description_left }}</span>
           </div>
       
           <div class="col-span-full md:col-span-4 hero-split__right">
-            <img class="bg-img" v-if="blok.bg_right" :src="blok.bg_right.filename" :alt="blok.bg_right.alt" alt="">
+            <img class="bg-img rounded-none" v-if="blok.bg_right" :src="blok.bg_right.filename" :alt="blok.bg_right.alt" alt="">
 
             <div class="top">
                 <span>{{ blok.top_description_right }}</span>
